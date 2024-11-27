cd9f0ebd63d8a56a107d56361cc040d6924ee0cb
Matteo Chiroli <chiroli.matteo@iisgalvanimi.edu.it>
2024-11-27 10:13:05 +0000

    push completo

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 #inizializza l'app Flask
 app = Flask(__name__)
@@ -15,3 +15,26 @@ if __name__ == '__main__':
 @app.route('/')
 def home():
     return render_template('index.html')
+
+lista_spesa = []
+# Definisce una route per il percorso '/aggiungi', che accetta solo richieste POST
+@app.route('/aggiungi', methods=['POST'])
+def aggiungi():
+    elemento = request.form['elemento']
+    if elemento:
+        lista_spesa.append(elemento)
+    return redirect(url_for('home'))
+
+@app.route('/rimuovi/<int:index>', methods=['POST'])
+def rimuovi(index):
+    if 0 <= index < len(lista_spesa):
+        lista_spesa.pop(index)
+    return redirect(url_for('home'))
+
+@app.route('/svuota_lista', methods=['POST'])
+def svuota_lista():
+    lista_spesa.clear()  # Svuota la lista
+    return redirect(url_for('home'))  # Reindirizza alla home
+
+if __name__ == '__main__':
+    app.run(debug=True)
